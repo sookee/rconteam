3860c64378e54790036e0a5cf209629c69944472
sookee <oasookee@gmail.com>
2013-02-18 08:24:47 +0000

    Moved TeamBalancer into its own header/source files.

---
+++ src/TeamBalancer.h
@@ -0,0 +1,92 @@
+#pragma once
+#ifndef _OA_TEAMBALANCER_H_
+#define _OA_TEAMBALANCER_H_
+
+/*
+ * TeamBalancer.h
+ *
+ *  Created on: Feb 17, 2013
+ *      Author: oasookee@gmail.com
+ */
+
+/*-----------------------------------------------------------------.
+| Copyright (C) 2013 SooKee oaskivvy@gmail.com                     |
+|               2013 w!ng                                          |
+'------------------------------------------------------------------'
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.
+
+http://www.gnu.org/licenses/gpl-2.0.html
+
+'-----------------------------------------------------------------*/
+
+#include <memory>
+
+#include "rcon.h"
+#include "types.h"
+#include "TeamPolicy.h"
+
+namespace oa {
+
+class TeamBalancer
+{
+private:
+	static const str prefix;
+	static const str suffix;
+
+	bool done = false;
+
+	const RCon rcon;
+	TeamPolicySPtr policy;
+
+	game g; // current snapshot
+
+	enum
+	{
+		ACT_CALL_TEAMS, ACT_REQUEST_PLAYER, ACT_PUTTEAM
+	};
+
+	typedef std::map<str, siz> action_map;
+	action_map actions;
+
+	void call_teams(siz num, char team);
+	void request_player(siz num, char team);
+	void putteam(siz num, char team);
+
+public:
+
+	TeamBalancer(const RCon& rcon)
+	: rcon(rcon), policy(TeamPolicy::create()) {}
+
+	/**
+	 * Get the current state of the server to reflect in the game object.
+	 *
+	 * @return True on success, false on failure.
+	 */
+	bool get_snapshot();
+
+	/**
+	 * Query the server over rcon for the value of the cvar 'rconteam_policy'
+	 * and select the appropriate TeamPolicy inplementation accordingly.
+	 */
+	void select_policy();
+
+	void run();
+};
+
+} // oa
+
+#endif /* _OA_TEAMBALANCER_H_ */
